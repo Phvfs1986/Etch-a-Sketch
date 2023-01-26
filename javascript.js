16fc19503a9d7c76331c8dc487af8da3cb166533
Paulo <phvfs19@gmail.com>
2023-01-26 10:21:15 -0300

    make it pretty. add random color button. fix layout.

--- javascript.js
+++ javascript.js
@@ -1,30 +1,58 @@
-let container = document.getElementById('container');
 let html = document.querySelector('html');
 let body = document.querySelector('body');
-let button = document.createElement('button');
+let main = document.getElementById('main');
+let buttonsContainer = document.getElementById('buttons');
+let container = document.getElementById('container');
+let btnColors = document.getElementById('btn-colors');
 
-button.textContent = 'Draw';
-button.style.padding = '20px';
-body.insertBefore(button, container);
+let draw = document.createElement('button');
+let drawRandom = document.createElement('button'); 
+let drawBlack = document.createElement('button');
 
-html.style.margin = '0';
-html.style.padding = '0';
-html.style.border = '0';
-body.style.margin = '0';
-body.style.padding = '0';
-body.style.border = '0';
-body.style.height = '100hv';
-body.style.textAlign = 'center';
-body.style.backgroundColor = 'black'
+draw.textContent = 'Draw Squares';
+draw.style.padding = '20px';
+draw.className = 'draw-button';
+buttonsContainer.appendChild(draw);
+
+drawBlack.textContent = 'Black';
+drawBlack.style.padding = '20px';
+drawBlack.style.backgroundColor = 'black';
+drawBlack.style.color = 'white';
+drawBlack.className = 'Black-button';
+btnColors.appendChild(drawBlack);
+
+drawRandom.textContent = 'Random';
+drawRandom.style.padding = '20px';
+drawRandom.className = 'rnd-colors-button';
+btnColors.appendChild(drawRandom);
+
+
 container.style.display = 'grid';
-container.style.height = '94.4vh';
+container.style.height = '40vh';
+container.style.width = '40vw'
 
-button = document.querySelector('button');
-button.addEventListener('click', e => {(
-    repeat(prompt('How many squares?')))});
+let elements = document.getElementsByClassName('cell');
+repeat(16);
+
+
+draw.addEventListener('click', e => {(
+    repeat(prompt('How many squares?')
+))});
+
+drawRandom.addEventListener('click', e => {(
+    addEvent(elements, randomizeColors())
+)});
+
+drawBlack.addEventListener('click', e => {
+    addEvent(elements, 'black')
+});
 
 function randomGen() {
     return Math.floor(Math.random() * 255);
+}
+function randomizeColors() {
+    drawRandom.style.backgroundColor = `rgb(${randomGen()}, ${randomGen()}, ${randomGen()})`
+    return drawRandom.style.backgroundColor;
 }
 function repeat(number) {
     container.innerHTML = '';
@@ -34,24 +62,22 @@ function repeat(number) {
     else{
         container.style.gridTemplate = `repeat(${number}, 1fr) / repeat(${number}, 1fr)`;
         for (let i = 1; i <= (number * number); i++) {
-            let cell = document.createElement('div');
+            const cell = document.createElement('div');
             cell.className = 'cell';
             cell.style.height = 'auto';
             cell.style.width = 'auto';
             cell.style.textAlign = 'center';
             //cell.style.backgroundColor = `rgb(${randomGen()}, ${randomGen()}, ${randomGen()})`;
-            cell.style.backgroundColor = 'black';
+            cell.style.backgroundColor = 'white';
             container.appendChild(cell);
         }
     }
-    let elements = document.getElementsByClassName('cell');
-    addEvent(elements);
+    addEvent(elements, 'black');
 }
-function addEvent(elements) {
+function addEvent(elements, color) {
     for (let element of elements) {
         element.addEventListener('mouseover', e => {
-            e.target.style.backgroundColor = 'white';
+            e.target.style.backgroundColor = color;
         })
     }
 }
-repeat(16);
